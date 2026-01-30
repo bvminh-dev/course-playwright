7f8800bfb2a9119bc1b06e1bf9f6eac1757bb14a
Bui Van Minh <bvminh.dev@gmail.com>
2026-01-30 17:54:47 +0700

    refactor to test action github

--- tests/courses/01.khai-niem-co-ban.spec.ts
+++ tests/courses/01.khai-niem-co-ban.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Lesson 1: Khái niệm cơ bản", () => {
-  test("Input", async ({ page }) => {});
+  test("Input", async ({ page }) => {
+    await expect(1).toEqual(1);
+  });
 });
